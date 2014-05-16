44d88fafd75da6d7b90a7fa274b43d32f8beef5d
Alex <alex@thumbcine.ma>
2014-05-17 01:38:06 +1000

    Attempt to get higher res artwork from soundcloud for larger devices

--- client/src/models/track.js
+++ client/src/models/track.js
@@ -13,7 +13,9 @@ module.exports = TrackModel = Backbone.Model.extend({
 
         var id = this.parse_url(this.get('play').href);
         this.get_track(id);
-        this.get_img();
+
+        if (App.data.window.width > 700) this.get_high_res_img();
+        else this.load_img();
 
         this.listenTo(this, 'change:src', this.loaded);
     },
@@ -33,18 +35,80 @@ module.exports = TrackModel = Backbone.Model.extend({
         });
     },
 
-    get_img: function() {
+    get_high_res_img: function() {
+        var self = this,
+            artist = this.get('artist').text,
+            title = this.get('title');
+
+        $.ajax({
+            url: 'http://api.soundcloud.com/tracks.json',
+            data: {
+                client_id: '3dea5f796277d33d7a3cfaf0536a65ef',
+                q: artist + ' ' + title,
+                limit: 5
+            },
+            success: function(tracks) {
+                var found_artists = {},
+                    found_tracks = {},
+                    artwork = false,
+                    artist_regex = new RegExp(artist, 'ig'),
+                    title_regex = new RegExp(title, 'ig');
+
+                // get a list of matching artists
+                found_artists = _.filter(tracks, function(track) {
+                    if (track.user.username.match(artist_regex)) return true;
+                    else return false;
+                });
+
+                // get a separate list of matching tracks, as a subset of found_artists
+                found_tracks = _.filter(found_artists, function(track) {
+                    if (track.title.match(title_regex)) return true;
+                    else return false;
+                });
+
+                // get the track artwork, or the artist avatar if no exact match
+                if (found_tracks.length == 1) {
+                    artwork = found_tracks[0].artwork_url;
+
+                    // if there isn't any track artwork, use the avatar
+                    artwork = found_artists[0].user.avatar_url;
+
+                } else if (found_tracks.length == 0 && found_artists.length > 0) {
+                    artwork = found_artists[0].user.avatar_url;
+                }
+
+                // if artwork has been found, chuck it in
+                if (artwork) {
+                    artwork = artwork.replace(/large/g, 't500x500');
+                    self.new_img(artwork);
+                }
+            },
+            error: function() {
+                // maybe do something I dunno
+            },
+            complete: function() {
+                // load whatever artwork we've got
+                self.load_img();
+            }
+        });
+    },
+
+    load_img: function() {
         var self = this,
             src = this.get('image').src,
             img = new Image();
 
         img.src = src;
-		$(img).load(function() {
+        $(img).load(function() {
             self.set('img_loading', false);
-		}).error(function (){
+        }).error(function (){
             self.set('img_loading', false);
             self.set('image', {src: ''});
         });
+    },
+
+    new_img: function(new_src) {
+        this.set('image', {src: new_src});
     },
 
     loaded: function() {
