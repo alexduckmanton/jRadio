82c0b88ee4f6ac8ea05d70525db08497174cbe3a
Alex <alex@thumbcine.ma>
2014-05-27 15:21:45 +1000

    Update some js the linter was complaining about

--- public/js/jRadio.js
+++ public/js/jRadio.js
@@ -15373,13 +15373,15 @@ module.exports = Helpers = function Helpers() {
 
     Handlebars.registerHelper('time', function(options) {
         var current = new Date(),
-            track = options.hash.time.split(/[- :T+]/), // needs to be split for ios
-            track = new Date(track[0], track[1]-1, track[2], track[3], track[4], track[5]),
-            track = new Date(track.getTime() - track.getTimezoneOffset()*60*1000),
-            diff = current.getTime() - track.getTime(),
             text = 'mins ago';
 
-        // get whole minutes
+        // get the track timestamp, and update for the timezone differences
+        var track = options.hash.time.split(/[- :T+]/); // needs to be split for ios
+        track = new Date(track[0], track[1]-1, track[2], track[3], track[4], track[5]);
+        track = new Date(track.getTime() - track.getTimezoneOffset()*60*1000);
+
+        // find the difference and change it into minutes
+        var diff = current.getTime() - track.getTime();
         diff = Math.floor( diff / 1000 / 60 );
 
         // don't show "1 mins ago" like an idiot
